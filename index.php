f321bceddfab01f056eb79bf288837172c8c4498
ylu021 <yichen_0@hotmail.com>
2017-03-13 03:41:17 -0400

    fix pdo

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php include 'head.php'; ?>
-    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-    <!-- <div id="player"></div> -->
-        <div class="row">
-          <div class="col-md-6 col-md-offset-3">
-                <p><input type="hidden" id="email"/></p>
-								<p><input type="text" id="searchtext" placeholder="Type Something..." autocomplete="off" class="form-control"/></p>
-								<p><input type="submit" id="search" value="Search" class="form-control btn btn-primary w100" style="display:none;"/></p>
-						<div id="searchresult">
-
-            </div>
-          </div>
-				</div>
-		</div>
+    <div class="row search">
+      <div class="search-bar">
+        <input type="text" id="searchtext" placeholder="Type Something..." autocomplete="off" class="form-control"/>
+        <input type="submit" id="search" value="Search" class="form-control btn-search"/>
+      </div>
+      <div id="searchresult" class="search-result"></div>
+    </div>
     <?php include 'script.php'; ?>
-
   </body>
 </html>
