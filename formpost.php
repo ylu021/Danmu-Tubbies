ed290bcb126e70c817539f22242873c3d2d15b05
ylu021 <yichen_0@hotmail.com>
2017-03-12 23:20:23 -0400

    fix pdo

--- formpost.php
+++ formpost.php
@@ -10,14 +10,14 @@
 				':sid' => urldecode($_POST['email']),
 				':sname' => urldecode($_POST['name'])
 		);
-		$query = "SELECT 1 from 'Tubbies_User' WHERE :sid=id AND :sname=name";
+		$query = 'SELECT * FROM "Tubbies_User" WHERE :sid=id AND :sname=name;';
 		$stmt = $db->prepare($query);
 		$stmt->execute($data);
 		$count = $stmt->rowCount();
 		echo "How many $count";
 		if($count==0){
 			echo "emptyz,";
-			$query = "INSERT INTO 'Tubbies_User' VALUES(:sid, :sname);";
+			$query = 'INSERT INTO "Tubbies_User" VALUES(:sid, :sname);';
 			$stmt = $db->prepare($query);
 			$result = $stmt->execute($data);
 			if($result){
