f93363b5a0b1b05c349343cf0a94c9b22417f3d8
ylu021 <yichen_0@hotmail.com>
2017-03-13 20:09:44 -0400

    making css

--- test.php
+++ test.php
@@ -37,7 +37,7 @@
 	      // var tag = document.createElement('script');
 
 	      // tag.src = "https://www.youtube.com/iframe_api"
-	      var firstScriptTag = document.getElementsByTagName('script')[0];
+	      // var firstScriptTag = document.getElementsByTagName('script')[0];
 	      // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 	      // 3. This function creates an <iframe> (and YouTube player)
