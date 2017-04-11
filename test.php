d5c0cfa075e563c587b0d54ee8a394ffd206d633
ylu021 <yichen_0@hotmail.com>
2017-04-11 00:40:39 -0400

    working locally, next step connect apache with nodejs

--- test.php
+++ test.php
@@ -39,7 +39,7 @@
 
     <script>
       console.log('port?', window.location.host)
-      var socket = io.connect( 'https://tubbies.herokuapp.com' )
+      var socket = io.connect( window.location.host )
       // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
 
